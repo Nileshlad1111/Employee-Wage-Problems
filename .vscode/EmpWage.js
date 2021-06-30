9a642ba81d8fa247c5bff5559282e377e0e1557c
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-30 12:50:45 +0530

    ADDED:To calculate employee wage part time and full time work hours

--- .vscode/EmpWage.js
+++ .vscode/EmpWage.js
@@ -1,9 +1,25 @@
-const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+let WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck = IS_ABSENT){
-    console.log("Employee is Absent");
-    return;
-}else{
-    console.log("Employee is Present");
-} 
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        console.log("Part time wage");
+        break;
+    case IS_PART_TIME:
+        empHrs = FULL_TIME_HOURS;
+        console.log("Full time wage");
+        break;
+    default:
+        empHrs=0;
+        console.log("emp absent");
+
+
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp wage :" + empWage);
