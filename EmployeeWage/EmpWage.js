ebb0f794aa3cdd0e46a53d1a0ef552132f5fc491
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-07-01 13:22:15 +0530

    ADDED:To total wage using array helper function

--- EmployeeWage/EmpWage.js
+++ EmployeeWage/EmpWage.js
@@ -1,3 +1,4 @@
+//UC6 Store daily wage in array
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -20,14 +21,37 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-while((totalEmpHrs<=MAX_HRS_IN_MONTH)&&(totalWorkingDays<NUM_OF_WORKING_DAYS)){
-    let empCheck = Math.floor(Math.random()*10)%3;
-    totalEmpHrs += getWorkingHours(empCheck);
-    totalWorkingDays++;
+function calcDailyWage(empHrs){
+    return empHrs*WAGE_PER_HOUR;
 }
 
-let empWage = totalEmpHrs*WAGE_PER_HOUR;
-console.log("Total working days: "+totalWorkingDays);
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let dailyWages = 0;
+let empDailyWageArray = new Array();
+while((totalEmpHrs<=MAX_HRS_IN_MONTH)&&(totalWorkingDays<NUM_OF_WORKING_DAYS)){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random()*10)%3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
+    empDailyWageArray.push(calcDailyWage(empHrs));
+}
+
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("Daily Wage Array: "+empDailyWageArray);
+console.log("UC6: Total working days: "+totalWorkingDays);
 console.log("Total emp hours = "+totalEmpHrs+" Total employee wage = "+empWage);
+
+//Array Helper Function
+//UC 7A Calculate total wage using forEach or reduce method
+let totalEmpWage = 0;
+function sum(dailyWage){
+    totalEmpWage += dailyWage;
+}
+empDailyWageArray.forEach(sum);
+console.log("UC7A - Total Days: "+totalWorkingDays+" Total hrs: "+totalEmpHrs+" Emp Wage:"+totalEmpWage);
+
+function totalWages(totalWage,dailyWage){
+    return totalWage + dailyWage;
+}
+console.log("UC7A - Emp Wage with reduce: "+empDailyWageArray.reduce(totalWages,0));
